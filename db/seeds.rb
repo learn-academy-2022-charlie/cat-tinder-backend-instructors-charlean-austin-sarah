dee73b868a50c8a131fd0d6f5b3edc82b5b3723a
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-06-30 10:30:53 -0700

    backend structure 7/7

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,44 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  # movies = Movie.create([
+  #   { name: "Star Wars" },
+  #   { name: "Lord of the Rings" }
+  #    ])
 #   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Gandalf", movie: movies.second)
+
+cats = [
+  {
+    name: 'Ketchup',
+    age: 5,
+    enjoys: 'Home Fried Burgers', 
+    image: 'https://thiscatdoesnotexist.com/'
+  },
+  {
+    name: 'Dog',
+    age: 3,
+    enjoys: 'Chasing cats', 
+    image: 'https://www.shutterstock.com/image-photo/fitness-cat-lifting-heavy-big-dumbbell-283555859'
+  },
+  {
+    name: 'Bolo',
+    age: 7,
+    enjoys: 'yarn', 
+    image: 'https://unsplash.com/photos/XMxn_NkvnR0'
+  },
+  {
+    name: 'Snowflake',
+    age: 12,
+    enjoys: 'Climbing trees', 
+    image: 'https://s1.1zoom.me/big0/603/Cats_Snow_Black_Paws_514726_1280x720.jpg'
+  }
+]
+
+
+cats.each do |attributes|
+  Cat.create attributes
+end
+
+p Cat.all
+p "Cat's Seeds worked out fine"
